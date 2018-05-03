09572ff1ac0ca4d3e871433217a7f335112fcc33
nobita <congson1293@gmail.com>
2018-05-03 15:54:58 +0700

    merge category

--- my_map.py
+++ my_map.py
@@ -1,9 +1,9 @@
 # -*- encoding: utf-8 -*-
 
-name2label = {u'Chinh tri Xa hoi':0, u'Doi song':1, u'Khoa hoc':2,
-              u'Kinh doanh':3, u'Phap luat':4, u'Suc khoe':5, u'The gioi':6,
-              u'The thao':7, u'Van hoa':8, u'Cong nghe':9}
+name2label = {u'Chinh tri Xa hoi':0, u'Doi song':1, u'Khoa hoc Cong nghe':2,
+              u'Kinh doanh':3, u'Suc khoe':4, u'The gioi':5,
+              u'The thao':6, u'Van hoa':7}
 
-label2name =  {0: u'Chinh tri Xa hoi', 1: u'Doi song', 2: u'Khoa hoc',
-               3:u'Kinh doanh', 4:u'Phap luat', 5:u'Suc khoe', 6:u'The gioi',
-               7:u'The thao', 8:u'Van hoa', 9:u'Cong nghe'}
+label2name =  {0: u'Chinh tri Xa hoi', 1: u'Doi song', 2: u'Khoa hoc Cong nghe',
+               3:u'Kinh doanh', 4:u'Suc khoe', 5:u'The gioi',
+               6:u'The thao', 7:u'Van hoa'}
