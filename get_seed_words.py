8c45e1db7b15dc3c9741d4c13b2d09f83f7160df
nobita <congson1293@gmail.com>
2018-05-04 15:49:45 +0700

    fix logic code in preprocessing

--- get_seed_words.py
+++ get_seed_words.py
@@ -6,6 +6,8 @@ from tokenizer.tokenizer import Tokenizer
 import utils
 import unicodedata
 import regex
+import my_map
+from pyvi.pyvi import ViPosTagger
 
 
 dataset = 'dataset/train'
@@ -30,9 +32,8 @@ def tokenizer_dataset():
             print('\r%s' % (file_path)),
             sys.stdout.flush()
             with open(file_path, 'r', encoding='utf-16') as fp:
-                content = r.run(unicodedata.normalize('NFKC', fp.read()))
-                # content = unicodedata.normalize('NFKC', fp.read())
-                content = tokenizer.predict(content)
+                content = unicodedata.normalize('NFKC', fp.read())
+                content = r.run(tokenizer.predict(content))
                 dir_name = utils.get_dir_name(file_path)
                 output_dir = os.path.join(tokenized_dataset, dir_name)
                 utils.mkdir(output_dir)
@@ -42,5 +43,43 @@ def tokenizer_dataset():
     print('')
 
 
+
+def count_tokens():
+    print('count tokens...')
+    statistic = {name : {} for name in my_map.name2label.keys()}
+    stack = os.listdir(tokenized_dataset)
+    print 'loading data in ' + dataset
+    while (len(stack) > 0):
+        file_name = stack.pop()
+        file_path = os.path.join(tokenized_dataset, file_name)
+        if (os.path.isdir(file_path)):
+            utils.push_data_to_stack(stack, file_path, file_name)
+        else:
+            print('\r%s' % (file_path)),
+            sys.stdout.flush()
+            with open(file_path, 'r', encoding='utf-8') as fp:
+                label = utils.get_dir_name(file_path)
+                for sen in fp:
+                    sen = sen.strip()
+                    tag = ViPosTagger.postagging(sen)
+                    tokens = [tag[0][i] for i in xrange(len(tag[0])) if tag[1][i] == u'N']
+                    update_count_tokens(statistic, label, tokens)
+
+
+def update_count_tokens(statistic, label, tokens):
+    for token in tokens:
+        try:
+            statistic[label][token] += 1
+        except:
+            try:
+                statistic[label].update({token : 1})
+            except:
+                statistic.update({label : {token : 1}})
+
+
+
+
+
 if __name__ == '__main__':
     tokenizer_dataset()
+    count_tokens()
--- preprocessing.py
+++ preprocessing.py
@@ -26,8 +26,8 @@ def load_dataset(dataset):
             print('\r%s' % (file_path)),
             sys.stdout.flush()
             with open(file_path, 'r', encoding='utf-16') as fp:
-                content = r.run(unicodedata.normalize('NFKC', fp.read()))
-                content = tokenizer.predict(content)
+                content = unicodedata.normalize('NFKC', fp.read())
+                content = r.run(tokenizer.predict(content))
                 dir_name = utils.get_dir_name(file_path)
                 list_samples[dir_name].append(content)
     print('')
@@ -37,8 +37,7 @@ def load_dataset(dataset):
 def load_dataset_ex(list_samples):
     result = []
     for sample in list_samples:
-        sample = r.run(sample)
-        sample = tokenizer.predict(sample)
+        sample = r.run(tokenizer.predict(sample))
         result.append(sample)
     return result
 
