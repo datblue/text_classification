d5cfcee7f5707dc674c1fde2305123ed23e56fc5
nobita <congson1293@gmail.com>
2018-07-02 23:03:45 +0700

    replace Maasking by Input layer

--- network.py
+++ network.py
@@ -1,12 +1,13 @@
 from keras.models import Sequential
-from keras.layers import LSTM, Dense, Flatten, Masking
+from keras.layers import LSTM, Dense, Flatten, Masking, Input
 
 
 def building_ner(num_lstm_layer, num_hidden_node, dropout,
                  time_step, vector_length, output_lenght):
     model = Sequential()
 
-    model.add(Masking(mask_value=0., input_shape=(time_step, vector_length)))
+    # model.add(Masking(mask_value=0., input_shape=(time_step, vector_length)))
+    model.add(Input(shape=(time_step, vector_length)))
 
     for i in xrange(num_lstm_layer):
         model.add(LSTM(num_hidden_node, return_sequences=True, dropout=dropout))
