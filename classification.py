6916a92f2f92cacec4e6b9b5e6a22ac1a85b2305
nobita <congson1293@gmail.com>
2018-07-03 17:07:23 +0700

    fix ValueError when compare a numpy array to None

--- classification.py
+++ classification.py
@@ -87,7 +87,7 @@ class classification:
     def training(self, data_train, data_test):
         n_labels = len(my_map.label2name)
         X_train, y_train = self.load_training_vector()
-        if X_train == None or y_train == None:
+        if X_train is None or y_train is None:
             samples_train = preprocessing.load_dataset_from_disk(data_train, self.max_length)
             X_train, y_train = self.prepare_data(samples_train)
             X_train = embedding.construct_tensor_word(X_train, self.max_length)
@@ -96,7 +96,7 @@ class classification:
         self.fit(X_train, y_train)
 
         X_test, y_test = self.load_testing_vector()
-        if X_test == None or y_test == None:
+        if X_test is None or y_test is None:
             samples_test = preprocessing.load_dataset_from_disk(data_test, self.max_length)
             X_test, y_test = self.prepare_data(samples_test)
             X_test = embedding.construct_tensor_word(X_test, self.max_length)
