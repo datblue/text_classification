fefc8092e5e7238a6c7e8f3c512d0eef1f89df09
nobita <congson1293@gmail.com>
2018-07-31 11:34:05 +0700

    using bidirectional

--- network.py
+++ network.py
@@ -1,16 +1,25 @@
 from keras.models import Sequential
-from keras.layers import LSTM, Dense, Flatten
+from keras.layers import LSTM, Dense, Flatten, Bidirectional
 
 
 def building_network(embedding_size, num_lstm_layer, num_hidden_node,
                      dropout, time_step, output_lenght):
     model = Sequential()
 
-    model.add(LSTM(num_hidden_node, return_sequences=True, dropout=dropout,
-                   input_shape=(time_step, embedding_size)))
+    # model.add(LSTM(num_hidden_node, return_sequences=True, dropout=dropout,
+    #                input_shape=(time_step, embedding_size)))
+    #
+    # for i in xrange(num_lstm_layer-1):
+    #     model.add(LSTM(num_hidden_node, return_sequences=True, dropout=dropout))
+
+    model.add(Bidirectional(LSTM(num_hidden_node, return_sequences=True,
+                                 recurrent_dropout=dropout,
+                                 input_shape=(time_step, embedding_size))))
 
     for i in xrange(num_lstm_layer-1):
-        model.add(LSTM(num_hidden_node, return_sequences=True, dropout=dropout))
+        model.add(Bidirectional(LSTM(num_hidden_node, return_sequences=True,
+                                     recurrent_dropout=dropout),
+                                merge_mode='concat'))
 
     model.add(Flatten())
 
